425cf3cbf151e8f0a661651d2b7f8dd96d26c216
Rafael Romão <rafaelromao@gmail.com>
2013-05-08 18:32:07 -0300

    Fixed javaEnum.OnOff to conform to the Proof of Concept

--- src/com/rafaelromao/javaStatePattern/javaEnum/OnOff.java
+++ src/com/rafaelromao/javaStatePattern/javaEnum/OnOff.java
@@ -2,16 +2,29 @@ package com.rafaelromao.javaStatePattern.javaEnum;
 
 public enum OnOff {
     ON {
-        public OnOff Switch() {
+        @Override
+        public String displayText() {
+            return "Ligado";
+        }
+
+        @Override
+        public OnOff swtch() {
             return OnOff.OFF;
         }
     },
 
     OFF {
-        public OnOff Switch() {
+        @Override
+        public String displayText() {
+            return "Desligado";
+        }
+
+        @Override
+        public OnOff swtch() {
             return OnOff.ON;
         }
     };
 
-    public abstract OnOff Switch();
+    public abstract OnOff swtch();
+    public abstract String displayText();
 }
